54bb3001170c546e61cb2c800956590ee7625890
Ryan Ly <ryan.ly@microfocus.com>
2022-03-04 15:06:33 -0500

    Rebase onto upstream v1.2.4

--- build.groovy
+++ build.groovy
@@ -5,7 +5,8 @@ properties([
                 string(defaultValue: params.BUILD_NODE ?: 'docker', description: 'Node to perform build on', name: 'BUILD_NODE', trim: false),
                 string(defaultValue: params.OPENSEARCH_PRODUCT ?: 'opensearch', description: 'Specify the product, e.g. opensearch or opensearch-dashboards', name: 'OPENSEARCH_PRODUCT', trim: false),
                 string(defaultValue: params.OPENSEARCH_VERSION ?: '1.0.0', description: 'Specify the version of opensearch eg: 1.0.0 or 1.0.0-beta1', name: 'OPENSEARCH_VERSION', trim: false),
-                string(defaultValue: params.OPENSEARCH_DOCKERFILE ?: 'dockerfiles/opensearch.microfocus.dockerfile', description: 'Specify the name of dockerfile for opensearch', name: 'OPENSEARCH_DOCKERFILE', trim: false)
+                string(defaultValue: params.OPENSEARCH_DOCKERFILE ?: 'dockerfiles/opensearch.microfocus.dockerfile', description: 'Specify the name of dockerfile for opensearch', name: 'OPENSEARCH_DOCKERFILE', trim: false),
+                string(defaultValue: params.OPENSEARCH_ARCH ?: 'x64', description: 'Specify one and only one architecture, e.g. x64 or arm64', name: 'OPENSEARCH_ARCH', trim: false)
         ]),
         buildDiscarder(logRotator(numToKeepStr: '10')),
         disableConcurrentBuilds()
@@ -30,8 +31,8 @@ node("${params.BUILD_NODE}") {
         timeout(time: 30, unit: 'MINUTES') {
             catchError(buildResult: 'FAILURE', stageResult: 'FAILURE') {
                 sh '''
-                    pushd release/docker
-                    sh ./build-image.sh -v ${OPENSEARCH_VERSION} -p ${OPENSEARCH_PRODUCT} -f ${OPENSEARCH_DOCKERFILE}
+                    pushd docker/release
+                    sh ./build-image-single-arch.sh -v ${OPENSEARCH_VERSION} -p ${OPENSEARCH_PRODUCT} -f ${OPENSEARCH_DOCKERFILE} -a ${OPENSEARCH_ARCH}
                     popd
                 '''
             }
@@ -48,7 +49,7 @@ node("${params.BUILD_NODE}") {
                     sh '''
                         export DOCKER_REGISTRY=arcsight-docker.svsartifactory.swinfra.net/${docker_repo}
                         docker login -u ${USERNAME} -p ${PASSWORD} $DOCKER_REGISTRY
-                        sh ./release/docker/build-docker.sh -t push -v ${OPENSEARCH_VERSION} -p ${OPENSEARCH_PRODUCT}
+                        sh ./docker/release/build-docker.sh -t push -v ${OPENSEARCH_VERSION} -p ${OPENSEARCH_PRODUCT}
                     '''
                 }
             }
@@ -60,7 +61,7 @@ node("${params.BUILD_NODE}") {
             catchError(buildResult: 'SUCCESS', stageResult: 'FAILURE') {
                 sh '''
                     export DOCKER_REGISTRY=arcsight-docker.svsartifactory.swinfra.net/"${docker_repo}"
-                    sh ./release/docker/build-docker.sh -t cleanup -v ${OPENSEARCH_VERSION} -p ${OPENSEARCH_PRODUCT}
+                    sh ./docker/release/build-docker.sh -t cleanup -v ${OPENSEARCH_VERSION} -p ${OPENSEARCH_PRODUCT}
                 '''
             }
         }
